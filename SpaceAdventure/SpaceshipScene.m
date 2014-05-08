9a37a9170fe6009ba6a9195760652627426969b0
Andy Leavitt <andy.l@truhearing.com>
2014-05-08 10:12:40 -0600

    Changed from image sprite to complex drawn sprite

--- SpaceAdventure/SpaceshipScene.m
+++ SpaceAdventure/SpaceshipScene.m
@@ -52,24 +52,24 @@
 }
 
 - (SKSpriteNode *)newSpaceship {
-//    SKSpriteNode *hull = [[SKSpriteNode alloc] initWithColor:[SKColor grayColor] size:CGSizeMake(64, 32)];
-    SKSpriteNode *hull = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-    hull.scale = 0.25;
-//    SKSpriteNode *light1 = [self newLight];
-//    light1.position = CGPointMake(-28.0, 6.0);
-//    [hull addChild:light1];
-//    
-//    SKSpriteNode *light1b = [self newLight];
-//    light1b.position = CGPointMake(-28.0, -6.0);
-//    [hull addChild:light1b];
-//    
-//    SKSpriteNode *light2 = [self newLight];
-//    light2.position = CGPointMake(28.0, 6.0);
-//    [hull addChild:light2];
-//    
-//    SKSpriteNode *light2b = [self newLight];
-//    light2b.position = CGPointMake(28.0, -6.0);
-//    [hull addChild:light2b];
+    SKSpriteNode *hull = [[SKSpriteNode alloc] initWithColor:[SKColor grayColor] size:CGSizeMake(64, 32)];
+//    SKSpriteNode *hull = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+//    hull.scale = 0.25;
+    SKSpriteNode *light1 = [self newLight];
+    light1.position = CGPointMake(-28.0, 6.0);
+    [hull addChild:light1];
+    
+    SKSpriteNode *light1b = [self newLight];
+    light1b.position = CGPointMake(-28.0, -6.0);
+    [hull addChild:light1b];
+    
+    SKSpriteNode *light2 = [self newLight];
+    light2.position = CGPointMake(28.0, 6.0);
+    [hull addChild:light2];
+    
+    SKSpriteNode *light2b = [self newLight];
+    light2b.position = CGPointMake(28.0, -6.0);
+    [hull addChild:light2b];
     hull.name = @"hull";
     return hull;
 }
@@ -80,16 +80,16 @@
     SKAction *action = nil;
     // Build up the movement action.
     if(self.moveForward == YES) {
-        action = [SKAction moveByX:0 y:10 duration:0.25];
+        action = [SKAction moveByX:0 y:10 duration:0.1];
     }
     if(self.moveBack == YES) {
-        action = [SKAction moveByX:0 y:-10 duration:0.25];
+        action = [SKAction moveByX:0 y:-10 duration:0.1];
     }
     if(self.moveLeft == YES) {
-        action = [SKAction moveByX:-10 y:0 duration:0.25];
+        action = [SKAction moveByX:-10 y:0 duration:0.1];
     }
     if(self.moveRight == YES) {
-        action = [SKAction moveByX:10 y:0 duration:0.25];
+        action = [SKAction moveByX:10 y:0 duration:0.1];
     }
     
     // Play the resulting action.
